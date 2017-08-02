cc586c361b0882b7c02a64780de4863d5609c8ee
Thomas Cheng <thomascheng81@gmail.com>
2017-08-01 22:47:17 -0400

    Render something to canvas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello, World");
+import { Canvas, Circle } from "@thomascheng/canvas-utils";
+
+const rootEl = document.getElementById("root");
+
+const canvas = new Canvas(rootEl);
+
+canvas.add(new Circle({
+  x: 100,
+  y: 100,
+  radius: 3,
+  fill: "#000"
+}));
+
+canvas.render();

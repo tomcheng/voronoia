fe79b053a669f140502db5df0bda90ca1a967fbe
Thomas Cheng <thomascheng81@gmail.com>
2017-08-04 17:14:07 -0400

    Do not mark lines as matched unless they overlap

--- src/utils/geometry.js
+++ src/utils/geometry.js
@@ -4,6 +4,7 @@ export const linesAreCollinear = (line1, line2) => {
   const tolerance = 2 * length(line1);
 
   return (
+    horizontallyOverlaps(line1, line2) &&
     Math.abs(crossProduct(vector(a2, a1), vector(a2, b1))) <= tolerance &&
     Math.abs(crossProduct(vector(b2, a1), vector(b2, b1))) <= tolerance
   );
@@ -32,3 +33,7 @@ const crossProduct = (u, v) => u.x * v.y - u.y * v.x;
 
 const length = ({ va, vb }) =>
   Math.sqrt((vb.x - va.x) ** 2 + (vb.y - va.y) ** 2);
+
+const horizontallyOverlaps = (line1, line2) =>
+  Math.min(line2.va.x, line2.vb.x) < Math.max(line1.va.x, line1.vb.x) &&
+  Math.max(line2.va.x, line2.vb.x) > Math.min(line1.va.x, line1.vb.x);
